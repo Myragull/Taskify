390d8171446c69c6c297130363a9404c7e9c04cd
Myra Gull <myragull749@gmail.com>
2025-03-23 04:34:32 -0700

    New commit

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 let userInput=document.querySelector(".user-input");
 let addBtn= document.querySelector(".add-btn");
 let displayTask=document.querySelector(".task_display-container");
-// let displayTaskParent =document.querySelector(".parent-of-task-display");
 
 
-function addTask(){
+function addTask(e){
+  // e.preventDefault();
 let uservalue=userInput.value;
 console.log(uservalue);
+// create a container to put the todo in
+const todoEntry= document.createElement('div');
+todoEntry.className = "todo";
+// if (uservalue) {
+  
+// }
 // created paragraph element inside displayTask
 const para=document.createElement('p');
 para.className = 'display-added-task';
 para.textContent=uservalue;
-displayTask.appendChild(para);
+todoEntry.appendChild(para);
 // created delete button
 const deleteBtn= document.createElement('Button');
 deleteBtn.className = 'delete-btn';
 deleteBtn.textContent= "Delete";
-displayTask.appendChild(deleteBtn);
-// displayTaskParent.appendChild(displayTask)!;
+todoEntry.appendChild(deleteBtn);
+displayTask.appendChild(todoEntry);
+// function for the deletebtn
+function deleteItem(){
+  para.remove();
+  deleteBtn.remove();
 }
+deleteBtn.addEventListener('click',deleteItem);
+userInput.value="";
+}
+
 
 addBtn.addEventListener("click", addTask);
 
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-      event.preventDefault();
+      // event.preventDefault();
       addTask();
     }
   });
