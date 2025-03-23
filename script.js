96176c0e06eed781f3074c17a2a0ba12a19dec7e
Myra Gull <myragull749@gmail.com>
2025-03-23 10:59:15 -0700

    updated js and other files

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let userInput=document.querySelector(".user-input");
 let addBtn= document.querySelector(".add-btn");
+let displayTask=document.querySelector(".task_display-container");
+// let displayTaskParent =document.querySelector(".parent-of-task-display");
+
 
 function addTask(){
 let uservalue=userInput.value;
 console.log(uservalue);
+// created paragraph element inside displayTask
+const para=document.createElement('p');
+para.className = 'display-added-task';
+para.textContent=uservalue;
+displayTask.appendChild(para);
+// created delete button
+const deleteBtn= document.createElement('Button');
+deleteBtn.className = 'delete-btn';
+deleteBtn.textContent= "Delete";
+displayTask.appendChild(deleteBtn);
+// displayTaskParent.appendChild(displayTask)!;
 }
 
 addBtn.addEventListener("click", addTask);
-// userInput.addEventListener("keydown",(e)=>{
-//     if(e.keycode===13){
-//         addTask();
-//     }
-// });
 
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
